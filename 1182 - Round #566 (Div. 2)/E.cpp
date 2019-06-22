ea8bb1865646fa5702bd50866198512f211ee6ad
trunghe <33113755+trunghe@users.noreply.github.com>
2019-06-22 14:12:30 +0700

    Updated 1182 E.cpp
    - Get input
    - Add transformation matrix

--- 1182 - Round #566 (Div. 2)/E.cpp
+++ 1182 - Round #566 (Div. 2)/E.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #define vec vector
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
 
+// Fermat's little theorem:
+//  a^(p-1) = 1 (mod p) when p is a prime number.
+
 // Constants
 typedef long long ll;
 const ll R = 1e9 + 7; // a^(R-1) = 1 (mod R)
@@ -63,7 +66,7 @@ public:
 
 // Prime related
 const int limit = 1 << 20;
-bool isPrime[limit] = {false, };
+bool notPrime[limit];
 vec<ll> primes;
 
 // Decompose given value to primes
@@ -99,6 +102,24 @@ ll modPow(ll a, ll x) {
   return res;
 }
 
+void sieve() {
+  notPrime[0] = notPrime[1] = true;
+  FOR(i, 2, limit) {
+    if (!notPrime[i]) {
+      primes.push_back(i);
+      for (int j = i + i; j < limit; j += i) {
+        notPrime[j] = true;
+      }
+    }
+  }
+}
+
+void printMatrix(Matrix &b) {
+  printf("  %lld %lld\n  %lld %lld\n",
+         b.num[0][0], b.num[0][1],
+         b.num[1][0], b.num[1][1]);
+}
+
 void testMatrix() {
   // Matrix functionality testing
   Matrix a1({{1, 2}, 
@@ -106,9 +127,7 @@ void testMatrix() {
   Matrix a2({{3, 2},
              {-1, 0}});
   Matrix b = a1 * a2;
-  printf("a1*a2 = \n  %lld %lld\n  %lld %lld\n",
-         b.num[0][0], b.num[0][1],
-         b.num[1][0], b.num[1][1]);
+  printf("a1*a2 = \n");
   // Expect:
   //    a1*a2 = 
   //      1 2
@@ -118,6 +137,27 @@ void testMatrix() {
 // Main fucntion
 int main() {
   testMatrix();
+  sieve();
+ 
+  // Get input and init params
+  ll n, f[4], c;
+  scanf("%lld %lld %lld %lld %lld", &n, &f[1], &f[2], &f[3], &c);
+  
+  // Let g_n denote c^n * f_n. Then we have g_n = g_{n−1} * g_{n−2} * g_{n−3}, 
+  // and thus for all n, g_n = (g_1)^{x_n} * (g_2)^{y_n} * (g_3)^{z_n}. Then the 
+  // recurrence satisfied by x_n, y_n, z_n is x_n = x_{n−1} + x_{n−2} + x_{n−3}, 
+  // and same for y, z. Now use matrix exponentiation modulo 10^9 + 6 to find the 
+  // reduced exponents, and this enables us to find g_n using the formula above. 
+  // Then we find c^{−n} * g_n using inverse of c_n, and thus we're done.
+  
+  // The required transformation matrix is given by the following three equations:
+  //      x_n = 1 * x_{n-1} + 1 * x_{n-2} + 1 * x_{n-3}
+  //  x_{n-1} = 1 * x_{n-1} + 0 * x_{n-2} + 0 * x_{n-3}
+  //  x_{n-2} = 0 * x_{n-1} + 1 * x_{n-2} + 0 * x_{n-3}
+  Matrix baseLogPropagate({{1, 1, 1},
+                           {1, 0, 0},
+                           {0, 1, 0}});
+  Matrix totalLogPropagate = baseLogPropagate ^ (n - 3);
   
   
   
